a1c71465ddf20fdc2e85644f9dd9ea30909f41bc
Skyler Sidner <malevolentdragon@gmail.com>
2015-05-20 13:44:01 -0500

    first commit after "rails new..."

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FantasyCommonNounSite::Application.config.secret_token = '75f5f0591193fc58aef4bbefc7dd93fa4da962361efc82305868022b7098f1f3c38f5f60c4ae93adf66736995bf6dd543d156c346a2d76bc2587ccca1934d5b1'
